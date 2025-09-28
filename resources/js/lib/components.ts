6d08299c13d6a9284e0292d1b1bb37eb32d3a8c0
Shmuel Bisberg <shmuelbisberg.acc@gmail.com>
2025-09-28 20:23:19 +0300

    fix: correct import style and format components array for consistency

--- resources/js/lib/components.ts
+++ resources/js/lib/components.ts
@@ -1,5 +1,10 @@
-import { NavItem } from "@/types";
+import { NavItem } from '@/types';
+import { Filter } from 'lucide-react';
 
 export const components: NavItem[] = [
-  // 
+    {
+        title: 'Laravel Query Builder',
+        href: route('mdx', 'laravel-query-builder'),
+        icon: Filter,
+    },
 ];
